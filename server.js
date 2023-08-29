739f0e4f206ece4dfe8e126ba81ba40284e8d519
Gaurav Sharma <gaurav.sharma1@xperi.com>
2023-08-29 14:29:03 +0530

    second commit

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Server running");
+const express=require("express");
+const app=express()
+
+
+app.get("/",(req,res)=>{
+    console.log("Hello This is welcome page");
+})
+app.listen(3000,(req,res)=>{
+    console.log("Server running...")
+})
