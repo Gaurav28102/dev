7146d8e0cf3e70ba759c24f4e6fd32f01eab1470
Gaurav28102 <143395580+Gaurav28102@users.noreply.github.com>
2023-08-31 14:49:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app=express()
 
 app.get("/",(req,res)=>{
     console.log("Hello This is welcome page");
+    res.send("Hello world");
 })
 app.listen(3000,(req,res)=>{
     console.log("Server running...")
