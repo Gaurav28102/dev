08fff50a2aae13e8fa4d11ee809c0e8372d6a656
Gaurav28102 <143395580+Gaurav28102@users.noreply.github.com>
2023-09-05 15:07:25 +0530

    Change port No to 8001

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("/",(req,res)=>{
     console.log("Hello This is welcome page");
     res.send("Hello world");
 })
-app.listen(3000,(req,res)=>{
-    console.log("Server running  again...Check 7")
+app.listen(8001,(req,res)=>{
+    console.log("Server running  again...Check 8")
 })
