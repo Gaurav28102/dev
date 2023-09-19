484d585d1f6af4ccde61584f433886eb0e68384f
Gaurav28102 <143395580+Gaurav28102@users.noreply.github.com>
2023-09-19 13:46:31 +0530

    port 8002

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("/",(req,res)=>{
     console.log("Hello This is welcome page");
     res.send("Hello world Change 1");
 })
-app.listen(8001,(req,res)=>{
+app.listen(8002,(req,res)=>{
     console.log("Server running  again...Check 10")
 })
